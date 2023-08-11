17f07fb3b201fb891b4dc9c4a1401fa4e91ded6e
Raiyani Krunal <krunalraiyani5@gmail.com>
2023-08-11 16:08:15 +0530

    add linkedin and github links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -30,17 +30,17 @@ const Footer = () => {
                 <div className="footer-icons">
                     <p className="social-media-title">Sicial Media</p>
                     <div className="social-icons">
+                        <a href="https://www.linkedin.com/in/krunalraiyani/" target="_blank"
+                        ><img src="img/linkedin-in.svg" className="linkedin" alt="linkedin"
+                            /></a>
+                        <a href="https://github.com/KrunalRaiyani" target="_blank"
+                        ><img src="img/github.svg" className="github" alt="github"
+                            /></a>
                         <a href="https://www.facebook.com/" target="_blank"
                         ><img src="img/facebook-f.svg" className="fb" alt="facebook"
                             /></a>
                         <a href="https://www.instagram.com/" target="_blank"
                         ><img src="img/instagram.svg" className="insta" alt="instagram"
-                            /></a>
-                        <a href="https://in.linkedin.com" target="_blank"
-                        ><img src="img/linkedin-in.svg" className="linkedin" alt="linkedin"
-                            /></a>
-                        <a href="https://twitter.com/i/flow/login" target="_blank"
-                        ><img src="img/twitter.svg" className="twitter" alt="twitter"
                             /></a>
                         <a href="https://www.youtube.com/" target="_blank"
                         ><img src="img/youtube.svg" className="yt" alt="youtube"
